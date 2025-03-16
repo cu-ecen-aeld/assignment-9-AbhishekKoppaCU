3669a63ab0e74bf4f37572d5d8d36977217ba4dd
AbhishekKoppa <abhishekkoppa@Ubuntu22.04-AESD.myguest.virtualbox.org>
2025-03-16 17:06:32 -0600

    hash change

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 2933e2aae8116b7a5a82c1856dab478f973fc257
+AESD_ASSIGNMENTS_VERSION = 9be8cc8fece9787472486dacb07cceed49015324
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,4 +1,4 @@
-AESDCHAR_VERSION = 2933e2aae8116b7a5a82c1856dab478f973fc257
+AESDCHAR_VERSION = 9be8cc8fece9787472486dacb07cceed49015324
 
 
 AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignment-3-AbhishekKoppaCU.git
