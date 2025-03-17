2759d00678657dac52df6bf4d737e9bd35d8ad9d
AbhishekKoppa <abhishekkoppa@Ubuntu22.04-AESD.myguest.virtualbox.org>
2025-03-16 20:21:28 -0600

    hash update and bugfix. Fulltest passes

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,7 +1,7 @@
-AESDCHAR_VERSION = 9be8cc8fece9787472486dacb07cceed49015324
+AESDCHAR_VERSION = 8d4cc89e90f1f66a4b7ddf12305fc1d9112479e5
 
 
-AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignment-3-AbhishekKoppaCU.git
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-AbhishekKoppaCU.git
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
 
